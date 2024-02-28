117501fc4dec6c74fb64a60f8b30d404662f2e2e
Zkimhom <Zkimhom18@gmail.com>
2024-02-28 20:35:45 +0800

    删除菜品业务代码开发

--- sky-server/src/main/java/com/sky/service/DishService.java
+++ sky-server/src/main/java/com/sky/service/DishService.java
@@ -1,7 +1,10 @@
 package com.sky.service;
 
 import com.sky.dto.DishDTO;
+import com.sky.dto.DishPageQueryDTO;
+import com.sky.result.PageResult;
 
+import java.util.List;
 
 
 public interface DishService {
@@ -10,4 +13,17 @@ public interface DishService {
      * @param dishDTO
      */
      void saveWithFlavor(DishDTO dishDTO);
+
+    /**
+     * 菜品分页查询
+     * @param dishPageQueryDTO
+     * @return
+     */
+    PageResult pageQuery(DishPageQueryDTO dishPageQueryDTO);
+
+    /**
+     * 菜品批量删除功能
+     * @param ids
+     */
+    void deleteBatch(List<Long> ids);
 }
