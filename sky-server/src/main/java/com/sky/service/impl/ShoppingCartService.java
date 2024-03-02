b843e548cf80d2b1e856c7d58b177cfc79065ea6
Zkimhom <Zkimhom18@gmail.com>
2024-03-02 21:47:02 +0800

    添加购物车业务代码开发

---
+++ sky-server/src/main/java/com/sky/service/impl/ShoppingCartService.java
@@ -0,0 +1,12 @@
+package com.sky.service.impl;
+
+import com.sky.dto.ShoppingCartDTO;
+
+public interface ShoppingCartService {
+
+    /**
+     * 添加购物车
+     * @param shoppingCartDTO
+     */
+    void addShoppingCart(ShoppingCartDTO shoppingCartDTO);
+}
