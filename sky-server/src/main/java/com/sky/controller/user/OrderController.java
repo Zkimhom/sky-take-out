921b1a3191f844b365c33ad6f0e33b702d8277f9
Zkimhom <Zkimhom18@gmail.com>
2024-03-05 20:42:05 +0800

    用户端订单功能代码开发

--- sky-server/src/main/java/com/sky/controller/user/OrderController.java
+++ sky-server/src/main/java/com/sky/controller/user/OrderController.java
@@ -2,10 +2,12 @@ package com.sky.controller.user;
 
 import com.sky.dto.OrdersPaymentDTO;
 import com.sky.dto.OrdersSubmitDTO;
+import com.sky.result.PageResult;
 import com.sky.result.Result;
 import com.sky.service.OrderService;
 import com.sky.vo.OrderPaymentVO;
 import com.sky.vo.OrderSubmitVO;
+import com.sky.vo.OrderVO;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
 import lombok.extern.slf4j.Slf4j;
@@ -48,5 +50,52 @@ public class OrderController {
         return Result.success(orderPaymentVO);
     }
 
+    /**
+     * 根据id查询订单详情
+     * @param id
+     * @return
+     */
+    @GetMapping("/orderDetail/{id}")
+    @ApiOperation("根据id查询订单详情1")
+    public Result<OrderVO> getById(@PathVariable Long id){
+        log.info("查询订单：{}详情",id);
+        OrderVO orderVO = orderService.getById(id);
+        log.info("订单详情：{}",orderVO);
+        return Result.success(orderVO);
+    }
 
+    /**
+     * 历史订单查询
+     * @param
+     * @return
+     */
+    @GetMapping("/historyOrders")
+    @ApiOperation("历史订单查询")
+    public Result<PageResult> historyOrders(int page,int pageSize,Integer status){
+        PageResult pageResult = orderService.pageQueryForUser(page,pageSize,status);
+        return Result.success(pageResult);
+    }
+
+    /**
+     * 取消订单
+     * @param id
+     * @return
+     */
+    @PutMapping("/cancel/{id}")
+    @ApiOperation("取消订单")
+    public Result cancel(@PathVariable Long id){
+        orderService.cancelById(id);
+        return Result.success();
+    }
+
+    /**
+     * 将商品重新加入购物车中
+     * @param id
+     * @return
+     */
+    @PostMapping("/repetition/{id}")
+    public Result OneMore(@PathVariable Long id){
+        orderService.repetition(id);
+        return Result.success();
+    }
 }
--- sky-server/src/main/java/com/sky/mapper/ShoppingCartMapper.java
+++ sky-server/src/main/java/com/sky/mapper/ShoppingCartMapper.java
@@ -45,4 +45,10 @@ public interface ShoppingCartMapper {
      */
     @Delete("delete from shopping_cart where id = #{id}")
     void deleteById(Long id);
+
+    /**
+     * 批量加入购物车数据
+     * @param shoppingCartList
+     */
+    void insertBatch(List<ShoppingCart> shoppingCartList);
 }
